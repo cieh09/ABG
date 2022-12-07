b66f3622bf760cd8d983dda7e3dc1ed80b7b7f68
Sophie <48855037+cieh09@users.noreply.github.com>
2022-12-07 16:00:07 -0500

    Add local storage for user
    Going to use for add/delete users and edit user info

--- ABG/ClientApp/src/app/components/login/login.component.ts
+++ ABG/ClientApp/src/app/components/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from 'src/app/common/user';
 import { SharedService } from 'src/app/services/shared.service';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { UserLogin } from 'src/app/common/userlogin';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +16,9 @@ export class LoginComponent implements OnInit {
   // login: Login = new Login();
   loginForm: FormGroup;
   user: User = new User();
-  constructor(
-    private http: HttpClient, 
-    private fb: FormBuilder, 
-    private shardService: SharedService,
-    private router: Router)  { }
+  userLogin: UserLogin = new UserLogin();
+
+  constructor(private http: HttpClient, private fb: FormBuilder, private shardService: SharedService)  { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -33,16 +31,23 @@ export class LoginComponent implements OnInit {
     if(this.loginForm.valid){
       console.log(this.loginForm.value);
 
-      this.shardService.validateUserInfo(this.loginForm.value).subscribe(data => {
-        this.user = data;
-        if(this.user.Name != null){
-          alert("Successful!");
-          this.router.navigateByUrl('');
+      this.userLogin.name = this.loginForm.value.name;
+      this.userLogin.password = this.loginForm.value.password;
+      this.shardService.validateUserInfo(this.userLogin).subscribe(data => {
+        this.user.Name = data.Name;
+        this.user.User_email = data.User_email;
+        this.user.User_id = data.User_id;
+        this.user.User_password = data.User_password;
+
+        if(data.User_id > 0){
+          
+          alert("Login successful.");
+        }else{
+          alert("Login failed.");
         }
-        else{
-          alert("No such user!");
-        }
-      });
+        localStorage.setItem('username', this.user.Name);
+        localStorage.setItem('email', this.user.User_email);
+      }); 
     }
     else{
       alert("Info is not valid!!");
